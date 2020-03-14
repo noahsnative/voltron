34ecc701eed207af371f927ce7f20300a2d2c783
Stanislav Khalash <stanislav.khalash@gmail.com>
2020-03-14 20:52:54 +0100

    Made the webhook server serve HTTPS

--- internal/injector/server/server_test.go
+++ internal/injector/server/server_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"crypto/tls"
 	"net/http"
 	"testing"
 
@@ -11,5 +12,14 @@ func TestWithPort(t *testing.T) {
 	t.Run("Should set the server address", func(t *testing.T) {
 		sut := New(http.NewServeMux(), WithPort(8080))
 		assert.Equal(t, ":8080", sut.Addr)
+	})
+}
+
+func TestWithCertificate(t *testing.T) {
+	t.Run("Should set the server address", func(t *testing.T) {
+		cert := tls.Certificate{}
+		sut := New(http.NewServeMux(), WithTLSCertificate(&cert))
+		assert.NotNil(t, sut.TLSConfig)
+		assert.ElementsMatch(t, []tls.Certificate{cert}, sut.TLSConfig.Certificates)
 	})
 }
