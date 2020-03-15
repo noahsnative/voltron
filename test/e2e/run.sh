01418fc8b6b9ae11deb7f5efb94b1ada12e0f193
Stanislav Khalash <stanislav.khalash@gmail.com>
2020-03-15 11:37:59 +0100

    Changed the way subscript is executed

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+scriptsLocation="$(dirname "${0}")"
+
 set -e
 trap 'kind delete cluster' EXIT
 
 curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64"
 chmod +x ./kind
 kind create cluster --image "kindest/node:v1.17.0"
-sh ${BASH_SOURCE[0]%/*}/webhook-create-signed-cert.sh --namespace default --service webhook --secret webhook-cert
+source "${scriptsLocation}"/webhook-create-signed-cert.sh --namespace default --service webhook --secret webhook-cert
 exit 0
 
 
