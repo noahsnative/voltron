22c67ea8d046fab36ef5de267381169ef56ecdc9
Stanislav Khalash <stanislav.khalash@gmail.com>
2020-03-15 11:48:35 +0100

    Refactored the run script

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+set -e
+
 scriptsLocation="$(dirname "${0}")"
 
-set -e
-trap 'kind delete cluster' EXIT
+function install_kind() 
+{
+  curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64"
+  chmod +x ./kind
+}
 
-curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64"
-chmod +x ./kind
+function cleanup()
+{
+  kind delete cluster
+  rm -rf ./kind
+}
+
+trap cleanup EXIT
+
+install_kind
+
 kind create cluster --image "kindest/node:v1.17.0"
+
 source "${scriptsLocation}"/webhook-create-signed-cert.sh --namespace default --service webhook --secret webhook-cert
+
 exit 0
-
-
-
 
