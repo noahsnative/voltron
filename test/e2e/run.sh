0d2dc8341c68477e87822415dc7e219e38110356
Stanislav Khalash <stanislav.khalash@gmail.com>
2020-03-15 10:55:52 +0100

    Changed the way kind is installed

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -3,7 +3,8 @@
 set -e
 trap 'kind delete cluster' EXIT
 
-go get -u sigs.k8s.io/kind
+curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64"
+chmod +x ./kind
 kind create cluster --image "kindest/node:v1.17.0"
 sh ${BASH_SOURCE[0]%/*}/webhook-create-signed-cert.sh --namespace default --service webhook --secret webhook-cert
 exit 0
